de784851893f24a90cc55e6b7769ac7c660a6833
ilorenzo <ivan.lorenzo@verint.com>
2023-07-18 21:35:14 +0700

    Java Coding Problems : Chapter 1 no 2 Solution

--- src/com/java/coding/problems/Main.java
+++ src/com/java/coding/problems/Main.java
@@ -2,6 +2,7 @@ package com.java.coding.problems;
 
 public class Main {
     public static void main(String[] args) {
-        DuplicateCharacters.count("aaiueo");
+        DuplicateCharacters.count("aaiueuoa"); //should be {a=3, u=2}
+        NonRepeatedCharacter.findFirst("ababcddefeg"); //should be c
     }
 }
---
+++ src/com/java/coding/problems/NonRepeatedCharacter.java
@@ -0,0 +1,35 @@
+package com.java.coding.problems;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * This is Solution to Chapter 1 no.2
+ * Finding the first non-repeated character:
+ * Write a program that returns the first non-repeated character from a given string.
+ */
+public class NonRepeatedCharacter {
+
+    public static void findFirst(String text) {
+        LinkedHashMap<String, Integer> characterMapping = new LinkedHashMap<>();
+        for (int i = 0; i < text.length(); i++) {
+            int numOfDuplicate;
+            String mappedChar = String.valueOf(text.charAt(i));
+            if (characterMapping.isEmpty() || !characterMapping.containsKey(mappedChar)) {
+                characterMapping.put(mappedChar, 1);
+            } else {
+                numOfDuplicate = characterMapping.get(mappedChar);
+                characterMapping.put(mappedChar, ++numOfDuplicate);
+            }
+        }
+        for (Map.Entry<String, Integer> entry : characterMapping.entrySet()) {
+            Integer value = entry.getValue();
+            if (value == 1) {
+                System.out.println("The first non repeated character is: " + entry.getKey());
+                break;
+            }
+        }
+    }
+}
