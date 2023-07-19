1765a3c5e9cdde0e989aad81ca0b945f36e204c2
ilorenzo <ivan.lorenzo@verint.com>
2023-07-19 22:52:27 +0700

    Java Coding Problems : Chapter 1 no 3 Solution

--- src/com/java/coding/problems/Main.java
+++ src/com/java/coding/problems/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         DuplicateCharacters.count("aaiueuoa"); //should be {a=3, u=2}
         NonRepeatedCharacter.findFirst("ababcddefeg"); //should be c
+        ReverseLetterAndWords.reverse("Hello World"); //should be dlroW olleH
     }
 }
