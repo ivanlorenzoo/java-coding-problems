111f19c6177490f6d6bd4b81c92294b562a8ed35
ivanlorenzoo <47498521+ivanlorenzoo@users.noreply.github.com>
2023-07-27 23:37:56 +0700

    Java Coding Problems : Chapter 1 no 11 (#4)
    Co-authored-by: ilorenzo <ivan.lorenzo@verint.com>

--- src/com/java/coding/problems/Main.java
+++ src/com/java/coding/problems/Main.java
@@ -5,15 +5,28 @@ import com.java.coding.problems.util.WordsGenerator;
 
 public class Main {
     public static void main(String[] args) {
+        /* Chapter 1 */
+        // no. 1
         DuplicateCharacters.count("aaiueuoa"); //should be {a=3, u=2}
+        // no. 2
         NonRepeatedCharacter.findFirst("ababcddefeg"); //should be c
+        // no. 3
         ReverseLetterAndWords.reverse("Hello World"); //should be dlroW olleH
+        // no. 4
         ContainsOnlyDigits.isOnlyDigits("1234x5"); //should be false
+        // no. 5
         CountVowelAndConsonant.count("aiueojkl"); //should be 5 vowels and 3 consonants.
+        // no. 6
         CountingOccurrenceCharacter.count("test123", 'a'); //should be 0 in given string
+        // no. 7
         StringConversionToNumber.convert("123.4");//should be 123.4
+        // no. 8
         RemoveWhiteSpace.removeSpace("Hello W o r l d");//should be HelloWorld
+        // no. 9
         JoinStringsDelimiter.joinString(WordsGenerator.generateCodingIsFun(), '\\'); //should be Coding\is\fun
+        // no. 10
         GeneratePermutation.getPermutation("ABC"); //should be ABC ACB BAC BCA CAB CBA
+        // no. 11
+        PalindromChecker.checkPalindrome("aba"); //should be true
     }
 }
