16cd185bfe23e70d875ed5504fe5159ba625a887
ilorenzo <ivan.lorenzo@verint.com>
2023-08-04 23:59:17 +0700

    Java Coding Problems : Chapter 1 no 19

--- src/com/java/coding/problems/chapter1/DeclareMultilineString.java
+++ src/com/java/coding/problems/chapter1/DeclareMultilineString.java
@@ -1,5 +1,10 @@
 package com.java.coding.problems.chapter1;
 
+/**
+ * This is Solution to Chapter 1 no.19
+ * Declaring multiline strings (text blocks):
+ * Write a program that declares multiline strings or text blocks.
+ */
 public class DeclareMultilineString {
 
     public static void printStringBlock() {
