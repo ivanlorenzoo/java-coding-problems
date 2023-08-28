ca88f10276c63e568d1560e28cd5a0e0938210f7
ivanlorenzoo <47498521+ivanlorenzoo@users.noreply.github.com>
2023-08-29 00:09:33 +0700

    Java Coding Problems : Chapter 2 no 41 (#37)
    Co-authored-by: ilorenzo <ivan.lorenzo@verint.com>

--- src/com/java/coding/problems/chapter2/CheckNull.java
+++ src/com/java/coding/problems/chapter2/CheckNull.java
@@ -3,19 +3,30 @@ package com.java.coding.problems.chapter2;
 import java.util.List;
 import java.util.Objects;
 
-/**
- * This is Solution to Chapter 2 no.40
- * Checking null references in functional style and imperative code:
- * Write a program that performs the null checks on the given references
- * in a functional style and imperative code.
- */
 public class CheckNull {
 
-    public static void checkWithFunctionalStyle(List<Integer> num) {
+    /**
+     * Chapter 2 no.40
+     * Checking null references in functional style and imperative code:
+     * Write a program that performs the null checks on the given references
+     * in a functional style and imperative code.
+     */
+    public static void checkWithFunctionalStyle(List<Integer> numbers) {
         // this method show the usage of Objects.isNull to check the object is null or not
         // there are other method like the usual == null operator
-        boolean test = num == null;
-        System.out.println(Objects.isNull(num));
+        System.out.println(Objects.isNull(numbers));
+    }
+
+    /**
+     * Chapter 2 no.41
+     * Checking null references and throwing a customized NullPointerException error:
+     * Write a program that performs the null checks on the given references
+     * and throws NullPointerException with custom messages.
+     */
+    public static void checkNullThrowNullPointer(List<Integer> numbers) {
+        if (numbers == null) {
+            throw new NullPointerException("Null list of number");
+        }
     }
 
 }
